5f30d6e4058b25d5c154588160bc88a3a72e7e38
dluecke <david.luecke@gmail.com>
2024-06-10 16:55:30 -0700

    adding option for trio binning read set

--- RunMerylMerqury-VPGRU.sh
+++ RunMerylMerqury-VPGRU.sh
@@ -3,11 +3,14 @@
 # RunMerylMerqury-VPGRU.sh runs meryl kmer analysis and merqury reads vs assembly tool
 
 usage() { 
-    echo "USAGE: $0 [-o|-k|-t|-g|-h] READS.FASTQ ASSEMBLY.FASTA"
+    echo "USAGE: $0 [-h|-o|-l|-t<-m-p>|-k|-c|-g] READS.FASTQ ASSEMBLY.FASTA"
     echo "  -o STRING FCS output directory, default ASSEMBLYNAME"
     echo "  -l FLAG write ReadsOnly files for leftovers"
+    echo "  -t FLAG do trio binning hapmer steps, REQUIRES:"
+    echo "    -m MATERNAL_READS.FASTA (glob OK)"
+    echo "    -p PATERNAL_READS.FASTA (glob OK)"
     echo "  -k INT kmer length, default 21"
-    echo "  -t INT threads, default 48"
+    echo "  -c INT cores, default 48"
     echo "  -g PATH to FlyComparativeGenomics git repo, default ~/FlyComparativeGenomics"
     echo "  -h FLAG print usage statement"
     exit 0
@@ -23,24 +26,34 @@ ASM_FN="$(basename $ASM_FASTA)"
 OUT_PREFIX="${ASM_FN%%.f*a}"
 # default run parameters
 READS_ONLY="" # empty string won't trigger ReadsOnly section
+TRIO=""
 K_LEN=21
 N_THREAD=48
 FCG_PATH=~/FlyComparativeGenomics
 
 # get options, including call usage if -h flag
-while getopts ":ho:lk:t:g:" arg; do
+while getopts ":ho:ltm:p:k:c:g:" arg; do
     case $arg in
-        o) # name for RunID and output directory, default filename
+        o) # name for RunID and output directory, default assembly filename
             OUT_PREFIX="${OPTARG}"
             ;;
         l) # flag for writing leftover ReadsOnly files
             # any string will do
             READS_ONLY="l" 
             ;;
+        t) # flag for trio binning hapmer analysis
+            TRIO="t"
+            ;;
+        m) # maternal read file used in trio binning
+            MATERNAL=${OPTARG}
+            ;;
+        p) # paternal read file used in trio binning
+            PATERNAL=${OPTARG}
+            ;;
         k) # kmer length for reads meryl
             K_LEN=${OPTARG}
             ;;
-        t) # number of threads for SLURM submission
+        c) # number of threads for SLURM submission
             N_THREAD=${OPTARG}
             ;;
         g) # path to annotation_tools/ git repo
@@ -55,6 +68,11 @@ done
 [[ -f $READS_FASTQ ]] || { echo "Can't find reads file ${READS_FASTQ}"; usage; }
 [[ -f $ASM_FASTA ]] || { echo "Can't find assembly file ${ASM_FASTA}"; usage; }
 [[ -d $FCG_PATH ]] || { echo "Can't find FlyComparativeGenomics repo at $FCG_PATH"; usage; }
+
+if [[ "$TRIO" == "t" ]]; then
+    find $MATERNAL -quit &> /dev/null || { echo "Can't find reads file ${MATERNAL}"; usage; }
+    find $PATERNAL -quit &> /dev/null || { echo "Can't find reads file ${PATERNAL}"; usage; }
+fi
 
 echo -e "Running meryl and merqury with k=${K_LEN} on reads:\n ${READS_FASTQ}"
 echo -e "and assembly:\n ${ASM_FASTA}\nRun in :\n $PWD"
