5dc4bf6f744e1d6f238f32294c31459a607bddd3
dluecke <david.luecke@gmail.com>
2024-09-26 10:51:17 -0700

    changing to use Ceres module smrtlink for pbmm2 instead of conda environment

--- RunPbmm2-VPGRU.sh
+++ RunPbmm2-VPGRU.sh
@@ -66,31 +66,11 @@ while getopts ":hspdo:q:t:" arg; do
     esac
 done
 
-
-# Ensure conda env pbmm2 activated before calling sbatch
-# function to configure access to pbmm2 conda environment (in vpgru projects)
-conda_env_dir() {
-    #conda config # writes ~/.condarc file, which will be modified
-    echo -e "\nenvs_dirs:\n  - /project/vpgru/.conda" >> ~/.condarc # add path to genomescope2 env
-    source activate -p /project/vpgru/.conda/pbmm2 || \
-        { echo "Problem activating conda env pbmm2" ; exit; }
-}
-
-# check for and activate conda environment pbmm2, 
-#  using conda_env_dir function to configure and activate if necessary 
-module load miniconda
-# check conda working (won't if still on head node)
-conda info --envs >/dev/null 2>&1 || { echo "Conda not working, need to switch to compute node?" ; usage; }
-
-[ -z "$(conda info --envs | grep pbmm2)" ] && \
-    conda_env_dir || \
-    source activate pbmm2
-
 # Print info to screen pre-submission
 echo -e "Starting pbmm2 to run minimap2, aligning reads in:\n ${HIFI_FOFN}"
 echo -e "onto assembly:\n ${ASM_FASTA}\nRun in :\n $PWD"
 echo -e "with output BAM file:\n ${OUT_PREFIX}.bam"
-echo "using pbmm2 conda environment in /project/vpgru/.conda/pbmm2"
+echo "using pbmm2 from smrtlink module"
 
 if [[ -n $SAM_OUT || -n $PAF_OUT ]]; then
     echo -e "\nUsing samtools to produce SAM file ${OUT_PREFIX}.sam"
@@ -117,7 +97,7 @@ sbatch --job-name="pbmm2-${OUT_PREFIX}" \
     -n ${N_THREAD} \
     -o "pbmm2.stdout-${OUT_PREFIX}.%j.%N" \
     -e "pbmm2.stderr-${OUT_PREFIX}.%j.%N" \
-    --export=ALL,ASM_FASTA=${ASM_FASTA},HIFI_FOFN=${HIFI_FOFN},N_THREAD=${N_THREAD},\
+    --export=ALL,ASM_FASTA=${ASM_FASTA},HIFI_FOFN=${HIFI_FOFN},N_THREAD=${N_THREAD},SLURM_QUEUE=${SLURM_QUEUE},\
 OUT_PREFIX=${OUT_PREFIX},SAM_OUT=${SAM_OUT},PAF_OUT=${PAF_OUT},GET_DEPTH=${GET_DEPTH} \
     ${GITLOC}/VPGRU-pbmm2_TEMPLATE.slurm
 
