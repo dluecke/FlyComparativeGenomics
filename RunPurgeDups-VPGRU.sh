34e664222df29a8ee6e41f88b86bf4cd4ba18fce
dluecke <david.luecke@gmail.com>
2024-10-01 11:41:48 -0700

    adding -m option to use manual cutoff

--- RunPurgeDups-VPGRU.sh
+++ RunPurgeDups-VPGRU.sh
@@ -8,6 +8,7 @@
 usage() { 
     echo "USAGE: $0 [-o|-t|-p|-h] PCTG_ASM.fa[sta] HIFI_ASM_ALIGN.paf[.gz]"
     echo "  -o STRING prefix for purged output, default PCTG_ASM-DeDup"
+    echo "  -m INT depth for manual cutoff, default NULL and auto-cutoff"
     echo "  -t INT threads, default 32"
     echo "  -p STRING slurm partition, default medium"
     echo "  -h FLAG print usage statement"
@@ -22,6 +23,7 @@ usage() {
 # last arg is the seq file
 PRI_ASM="${@: -2:1}"
 PAF_IN="${@: -1}"
+M_CUT=""
 N_THREAD=32
 QUEUE="medium"
 ASM_NAME=$(basename $PRI_ASM)
@@ -29,10 +31,13 @@ ASM_NAME=$(basename $PRI_ASM)
 [[ "$ASM_NAME" == *".fasta" ]] && PD_OUT="${ASM_NAME%%.fasta}-DeDup"
 
 # get options, including call usage if -h flag
-while getopts ":hp:o:t:" arg; do
+while getopts ":hp:o:t:m:" arg; do
     case $arg in
         o) # name for RunID and output directory, default filename
             PD_OUT="${OPTARG}"
+            ;;
+        m) # cutoff value for manual cutoff, flag to run manual cutoff
+            M_CUT=${OPTARG}
             ;;
         t) # number of threads for SLURM submission
             N_THREAD=${OPTARG}
@@ -56,8 +61,13 @@ done
 echo -e "Starting purge_dups on assembly:\n ${PRI_ASM}"
 echo -e "using HiFi alignment:\n ${PAF_IN}\nRun in :\n $PWD"
 echo -e "with output file prefix:\n ${PD_OUT}"
+
+if [[ -n $M_CUT ]]; then
+    echo "Manual cutoff with -m $M_CUT"
+fi
+
 echo -e "using purge_dups and minimap2 modules\n"
-echo -e "Submitting to $QUEUE partition with $N_THREAD threads"
+echo -e "Submitting to $QUEUE partition with $N_THREAD threads\n"
 
 # Launch SLURM script via template in Git Repo
 # location of Git Repo for this script (and SLURM template)
@@ -70,7 +80,7 @@ sbatch --job-name="purge_dups-${PD_OUT}" \
     -o "pd.${PD_OUT}.%j.%N.stdout" \
     -e "pd.${PD_OUT}.%j.%N.stderr" \
     --export=ALL,PRI_ASM=${PRI_ASM},PAF_IN=${PAF_IN},\
-PD_OUT=${PD_OUT},N_THREAD=${N_THREAD},QUEUE=${QUEUE} \
+PD_OUT=${PD_OUT},N_THREAD=${N_THREAD},QUEUE=${QUEUE},M_CUT=${M_CUT} \
     ${GITLOC}/VPGRU-PurgeDups_TEMPLATE.slurm
 
 
