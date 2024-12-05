4cc64f7a246c339110e342e7fd2b9303bc4570c3
dluecke <david.luecke@gmail.com>
2024-12-05 11:50:03 -0800

    bug from errant paste

--- get_depth_by_window.sh
+++ get_depth_by_window.sh
@@ -16,6 +16,7 @@ awk -v win=$WINDOW -v OFS='\t' '{w=int($2/win)} {print $0, $1"-"w}' $DEPTHFILE |
     awk -v OFS='\t' '{
         split($1, arr, "-")
     } END {
-        print length(arr[1]), arr[1], arr[2], $00015}' |\
+        print length(arr[1]), arr[1], arr[2]
+    }' |\
     sort -k1,1n -k2,2 -k3,3n | cut -f2-6 > $OUTFILE
 
