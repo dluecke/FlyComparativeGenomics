8cbc4aea75c5161ecf6e3db58f1aaa68efad1115
dluecke <david.luecke@gmail.com>
2024-12-05 11:25:11 -0800

    awk lines to get average depth in 100kb windows from samtools depth.tsv file

---
+++ get_depth_by_window.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get depth by windows
+WINDOW=100000
+DEPTHFILE=$1
+OUTFILE=$(echo $DEPTHFILE | sed 's/depth/depth_by_windows/')
+awk -v win=$WINDOW -v OFS='\t' '{w=int($2/win)} {print $0, $1"-"w}' $DEPTHFILE | \
+    awk -v OFS='\t' '{
+        sum[$4] += $3; count[$4]++
+    } END { 
+        n=asorti(count, indices); 
+        for (i=1; i<=n; i++) {
+            print indices[i], count[indices[i]], sum[indices[i]]/count[indices[i]]
+        } 
+    }' | \
+    awk -v OFS='\t' '{
+        split($1, arr, "-")
+    } END {
+        print length(arr[1]), arr[1], arr[2], $00015}' depth_windows.tsv |\
+    sort -k1,1n -k2,2 -k3,3n | cut -f2-6 > $OUTFILE
+
