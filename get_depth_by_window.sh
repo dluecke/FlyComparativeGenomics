5f2471655baa14cc829add15c29115e68d006deb
dluecke <david.luecke@gmail.com>
2024-12-05 12:06:23 -0800

    skip temp file

--- get_depth_by_window.sh
+++ get_depth_by_window.sh
@@ -12,12 +12,11 @@ awk -v win=$WINDOW -v OFS='\t' '{w=int($2/win)} {print $0, $1"-"w}' $DEPTHFILE |
         for (i=1; i<=n; i++) {
             print indices[i], count[indices[i]], sum[indices[i]]/count[indices[i]]
         } 
-    }' > temp_depth_windows.tsv
-
-awk -v OFS='\t' '{
+    }' | \
+    awk -v OFS='\t' '{
         split($1, arr, "-")
     } {
         print length(arr[1]), arr[1], arr[2], $0
-    }' temp_depth_windows.tsv | \
+    }' | \
     sort -k1,1n -k2,2 -k3,3n | cut -f2-6 > $OUTFILE
 
