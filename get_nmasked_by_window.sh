3a127c49e0f2d36da917c67f198211ed0ba34b72
dluecke <david.luecke@gmail.com>
2024-12-10 16:03:03 -0800

    non-inclusinve end points

--- get_nmasked_by_window.sh
+++ get_nmasked_by_window.sh
@@ -14,7 +14,7 @@ while read SCAF; do
     N_WINDOWS=$((LENGTH/WINDOW))
     for i in $(seq 0 $N_WINDOWS); do 
         BEG=$((i*WINDOW))
-        END0=$(((i+1)*WINDOW-1))
+        END0=$(((i+1)*WINDOW))
         END=$((END0 < LENGTH ? END0 : LENGTH))
         WINDOW_LENGTH=$(seqtk subseq $HMASM \
             <(echo -e "$SCAF\t$BEG\t$END") | \
