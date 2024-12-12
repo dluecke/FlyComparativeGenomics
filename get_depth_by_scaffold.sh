9e805e8c5c3da3fee84648fc07e54a1be02a9cf7
dluecke <david.luecke@gmail.com>
2024-12-12 12:59:42 -0800

    script to get average depth per scaffold

---
+++ get_depth_by_scaffold.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# get depth by scaffold
+# takes samtools depth output TSV with 3 columns: seqid position depth
+# outputs depth_by_scaffold.tsv with 3 columns: seqid n_positions sum(depth)/n_positions
+DEPTHFILE=$1
+
+if [[ $DEPTHFILE =~ "depth" ]]; then
+    OUTFILE=$(echo $DEPTHFILE | sed "s/depth/depth_by_scaffold/")
+else
+    OUTFILE="$DEPTHFILE.depth_by_scaffold.tsv"
+fi
+
+awk -v OFS='\t' '{
+   sum[$1] += $3; count[$1]++
+} END { 
+    n=asorti(count, indices); 
+    for (i=1; i<=n; i++) {
+        print indices[i], count[indices[i]], sum[indices[i]]/count[indices[i]]
+    } 
+}' $DEPTHFILE > $OUTFILE
+
