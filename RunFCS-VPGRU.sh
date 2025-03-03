4c1e40d166679f535b1996b192d1144e8a7c1cdf
dluecke <david.luecke@gmail.com>
2025-03-03 15:53:40 -0800

    change output directory name

--- RunFCS-VPGRU.sh
+++ RunFCS-VPGRU.sh
@@ -22,7 +22,7 @@ usage() {
 # last 2 args the reference and query seq files
 SEQFILE="${@: -1}"
 #OUT_DIR="$(basename $SEQFILE)-FCSout"
-OUT_DIR="${SEQFILE%.*}"
+OUT_DIR="FCS-${SEQFILE%.*}"
 # default run parameters
 CLEAN=""
 N_THREAD=32
