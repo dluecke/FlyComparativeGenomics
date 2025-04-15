2e65bd53da271f128cff4f699d941662322b9196
dluecke <david.luecke@gmail.com>
2025-04-15 10:56:24 -0700

    fix samtools coordinate span syntax, change default span to 5kb

--- get_spanning_reads.sh
+++ get_spanning_reads.sh
@@ -6,7 +6,7 @@
 #  MAPPING.bam file with reads mapped to sequence
 #  target SEQuence (contig/scaffold/chromosome)
 #  target COORDinate (bp on target sequence)
-#  SPAN length (distance from target coordinate required to cover on both sides, default 1kb)
+#  SPAN length (distance from target coordinate required to cover on both sides, default 5kb)
 # writes:
 #  MAPPING-SEQ_COORDspanSPANbp.sam file with all reads spanning target coordinate +/- span distance
 
@@ -16,13 +16,13 @@
 IN_BAM=$1
 TARGET_SEQ=$2
 TARGET_COORD=$3
-# default span lenth to 1kb if not provided
-[[ -n $4 ]] && SPAN_LENGTH=$4 || SPAN_LENGTH=1000
+# default span lenth to 5kb if not provided
+[[ -n $4 ]] && SPAN_LENGTH=$4 || SPAN_LENGTH=5000
 
 # output filename
 OUT_SAM="${IN_BAM%.*}-${TARGET_SEQ}_${TARGET_COORD}span${SPAN_LENGTH}bp.sam"
 
 # samtools view to extract reads covering target seq/coord
-samtools view $IN_BAM ${TARGET_SEQ}:${TARGET_COORD}:${TARGET_COORD} |\
+samtools view $IN_BAM ${TARGET_SEQ}:${TARGET_COORD}-${TARGET_COORD} |\
 # awk to filter for reads covering full span
 awk -v t=$TARGET_COORD -v s=$SPAN_LENGTH '$4 <= t-s && $4+$9 >= t+s' > $OUT_SAM
