93792ada494430380f3c11d383abf2467ddbf97d
dluecke <david.luecke@gmail.com>
2025-06-12 08:33:12 -0700

    do all steps

--- minimap2-ONT.sh
+++ minimap2-ONT.sh
@@ -9,6 +9,6 @@ ONT=$2
 
 OUT_NAME=${REF%.*}-${ONT%.*}-mm2
 
-#minimap2 -a -k19 -w19 -U50,500 -g10k $REF $ONT > $OUT_NAME.sam
+minimap2 -a -k19 -w19 -U50,500 -g10k $REF $ONT > $OUT_NAME.sam
 
 paftools.js sam2paf ${OUT_NAME}.sam | gzip -c - > ${OUT_NAME}.paf.gz
