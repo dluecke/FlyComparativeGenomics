11942de6afd8ac233c52341d7a13ddf81859e0d2
dluecke <david.luecke@gmail.com>
2025-06-13 14:56:35 -0700

    script for ragtag scaffolding step

---
+++ ragtag_scaffold.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ragtag_scaffold.sh runs ragtag scaffold
+
+# USAGE: ragtag_scaffold.sh REFERENCE.fa QUERY.fa
+
+module load ragtag/2.1.0
+
+REF=$1
+QRY=$2
+
+# split the scaffolds first, prevent trailing Ns in output from correct
+ragtag.py splitasm $QRY > ${QRY%.*}.split.fa
+
+# run correct to break contigs if alignment and reads support
+# -r to estimate gap size
+ragtag.py -r scaffold $REF ${QRY%.*}.split.fa
