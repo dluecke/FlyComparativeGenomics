d7b383cf57a51c9fba252001161ef819b710c91c
dluecke <david.luecke@gmail.com>
2025-06-13 13:37:44 -0700

    add splitasm step

--- ragtag_correct.sh
+++ ragtag_correct.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # ragtag_correct.sh runs ragtag correct with reference reads
-# needs at least 16 threads
+# needs at least 32 threads
 
-# USAGE: ragtag_correct.sh REFERENCE.fa QUERY.fa QUERY_READS.f?
+# USAGE: ragtag_correct.sh REFERENCE.fa QUERY.fa QUERY_READS.fa|fq
 
 module load ragtag/2.1.0
 
@@ -11,4 +11,8 @@ REF=$1
 QRY=$2
 READS=$3
 
-ragtag.py correct -R $READS -T corr -t 16 $REF $QRY
+# split the scaffolds first, prevent trailing Ns in output from correct
+ragtag.py splitasm $QRY > ${QRY%.*}.split.fa
+
+# run correct to break contigs if alignment and reads support
+ragtag.py correct -R $READS -T corr -t 32 $REF ${QRY%.*}.split.fa
