7e0133d2ae48fa1c5c094271333fabe80d4de723
dluecke <david.luecke@gmail.com>
2025-06-16 11:38:39 -0700

    remove splitasm step

--- ragtag_scaffold.sh
+++ ragtag_scaffold.sh
@@ -9,9 +9,6 @@ module load ragtag/2.1.0
 REF=$1
 QRY=$2
 
-# split the scaffolds first, prevent trailing Ns in output from correct
-ragtag.py splitasm $QRY > ${QRY%.*}.split.fa
-
 # run correct to break contigs if alignment and reads support
 # -r to estimate gap size, -u to tag unmodified sequences (better for AGP)
-ragtag.py scaffold -r -t 32 $REF ${QRY%.*}.split.fa
+ragtag.py scaffold -r -t 32 $REF $QRY
