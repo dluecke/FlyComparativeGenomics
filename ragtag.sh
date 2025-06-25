1093362e863a3275ac2c8a2d20653b6ca26f6e5c
dluecke <david.luecke@gmail.com>
2025-06-25 13:20:39 -0700

    add --debug to correct

--- ragtag.sh
+++ ragtag.sh
@@ -14,13 +14,14 @@ QRY=$2
 READS=$3
 
 # run correct to break contigs if alignment and reads support
+# --debug to provide more info on breaks
 # --intra only breaks when query maps to single ref sequence, 
 #    prevents splitting out unplaced reference scaffolds
 # -f minimum alignment length to consider
 # -u to tag unmodified sequences (better for AGP)
 # -T corr for high quality long reads
 # output file (default): ragtag_output/ragtag.correct.fasta
-ragtag.py correct --intra -f 5000 -u -R $READS -T corr -t 32 $REF $QRY
+ragtag.py correct --debug --intra -f 5000 -u -R $READS -T corr -t 32 $REF $QRY
 
 # run scaffold to reorder corrected QRY scaffolds
 # -r to estimate gap size, -u to tag unmodified sequences (better for AGP)
