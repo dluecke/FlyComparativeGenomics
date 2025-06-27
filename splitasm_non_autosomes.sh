44fd0dc1a430b504c900b2a96308059d7d88301b
dluecke <david.luecke@gmail.com>
2025-06-27 02:15:27 -0700

    variable name

--- splitasm_non_autosomes.sh
+++ splitasm_non_autosomes.sh
@@ -19,14 +19,14 @@ samtools faidx $IN_ASM
 
 sort $IN_ASM.fai -nr -k2,2 | \
     cut -f1 | \
-    head -n $N_CHR \
+    head -n $N_AUTOSOME \
     > scaffolds-autosomes.list
 
 seqtk subseq $IN_ASM scaffolds-autosomes.list > ${IN_ASM%.*}.autosomes.fa
 
 sort $IN_ASM.fai -nr -k2,2 | \
     cut -f1 | \
-    tail -n $((N_CHR+1)) \
+    tail -n $((N_AUTOSOME+1)) \
     > scaffolds-rest.list
 
 seqtk subseq $IN_ASM scaffolds-rest.list > ${IN_ASM%.*}.rest.fa
