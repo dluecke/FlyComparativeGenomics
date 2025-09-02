cf98f0dfbea89b6f2b137f4df42e9096be6f111b
dluecke <david.luecke@gmail.com>
2025-09-02 13:31:18 -0700

    shortcut to make diploid assembly from 2 haps

---
+++ combine_haps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# combine_haps.sh takes hap1 and hap2 assemblies, adds hap tag to seqeunce headers, and prints both to stdout
+
+# USAGE: combine_haps.sh hap1.fa hap2.fa > diploid.fa
+
+[ $# -eq 2 ] || {echo "USAGE: combine_haps.sh hap1.fa hap2.fa > diploid.fa"; exit; }
+
+echo "passed check"
