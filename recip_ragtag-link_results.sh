148372fe998fb0c84f97532f215b88167619fddd
dluecke <david.luecke@gmail.com>
2025-09-10 11:26:14 -0700

    need actual file paths not snakemake variables

--- recip_ragtag-link_results.sh
+++ recip_ragtag-link_results.sh
@@ -3,14 +3,14 @@
 # recip_ragtag-link_results.sh collects final output from 
 # makes RT_final/ output directory and writes s links to final output assemblies
 
-mkdir RT_final
+mkdir -p RT_final
 cd RT_final
-ln -s ../{input.FpA} Fp_RTfinal.fa
-ln -s ../{input.Fh1} Fh1_RTfinal.fa
-ln -s ../{input.Fh2} Fh2_RTfinal.fa
-ln -s ../{input.MpA} Mp_RTfinal.fa
-ln -s ../{input.Mh1} Mh1_RTfinal.fa
-ln -s ../{input.Mh2} Mh2_RTfinal.fa
-ln -s ../{input.FpB} ALT-Fp_RTfinal.fa
-ln -s ../{input.MpB} ALT-Mp_RTfinal.fa
+ln -s ../pri_haps-female/second_hap1/pri/ragtag_output-round2/ragtag.scaffold.fasta Fp_RTfinal.fa
+ln -s ../pri_haps-female/second_hap1/hap1/ragtag_output-round2/ragtag.scaffold.fasta Fh1_RTfinal.fa
+ln -s ../pri_haps-female/second_hap2/hap2/ragtag_output-round2/ragtag.scaffold.fasta Fh2_RTfinal.fa
+ln -s ../pri_haps-male/second_hap1/pri/ragtag_output-round2/ragtag.scaffold.fasta Mp_RTfinal.fa
+ln -s ../pri_haps-male/second_hap1/hap1/ragtag_output-round2/ragtag.scaffold.fasta Mh1_RTfinal.fa
+ln -s ../pri_haps-male/second_hap2/hap2/ragtag_output-round2/ragtag.scaffold.fasta Mh2_RTfinal.fa
+ln -s ../pri_haps-female/second_hap2/pri/ragtag_output-round2/ragtag.scaffold.fasta ALT-Fp_RTfinal.fa
+ln -s ../pri_haps-male/second_hap2/pri/ragtag_output-round2/ragtag.scaffold.fasta ALT-Mp_RTfinal.fa
 cd ..
