f2fdd80ecf8fbeb0ef744201641091a7e69bbc8e
dluecke <david.luecke@gmail.com>
2025-09-24 10:29:12 -0700

    skip round 1 if good R1 output found

--- Reciprocal_RagTag.sh
+++ Reciprocal_RagTag.sh
@@ -38,64 +38,76 @@ GIT_REPOS=~
 
 # ROUND 1 - rescaffold QRY_ASM based on REF_ASM
 
-# Alignment between ref and qry for RagTagShield
-REFASM_FILENAME=$(basename $REF_ASM)
-QRYASM_FILENAME=$(basename $QRY_ASM)
-ALIGNMENT1_NAME=${REFASM_FILENAME%.*}-vs-${QRYASM_FILENAME%.*}-c5000
+# Check for prior successful round1 run using ragtag_round1-pct_chrs.txt, if round1 successful will return integer
+R1_CHR_N=$(grep --no-messages ChromNumber ragtag_round1-pct_chrs.txt | cut -f2)
 
-echo -e "\nRunning genome alignment to identify regions to preserve in $QRY_ASM"
-echo -e "Commands:\n nucmer -p ${ALIGNMENT1_NAME} --threads=32 -c 5000 ${REF_ASM} ${QRY_ASM}"
-echo -e " show-coords ${ALIGNMENT1_NAME}.delta > ${ALIGNMENT1_NAME}.coords\n"
+# skip round 1 if integer round 1 chromosome count and round 1 output exists
+if [[ $R1_CHR_N =~ $re_int && -f ragtag_output-round1/ragtag.scaffold.fasta ]]; then
 
-nucmer -p ${ALIGNMENT1_NAME} --threads=32 -c 5000 ${REF_ASM} ${QRY_ASM}
-show-coords ${ALIGNMENT1_NAME}.delta > ${ALIGNMENT1_NAME}.coords
+  echo -n "\nFound output from successful round 1."
+  echo "Skipping to round 2 using ${QRY_ASM} and ragtag_output-round1/ragtag.scaffold.fasta"
 
-# RagTagShield for first round of ragtag
-echo -e "\nWriting GFF for regions to preserve in $QRY_ASM"
-echo -e "Command:\n Rscript $GIT_REPOS/FlyComparativeGenomics/RagTagShield-writeGFF.R ${ALIGNMENT1_NAME}.coords"
+else
 
-Rscript $GIT_REPOS/FlyComparativeGenomics/RagTagShield-writeGFF.R ${ALIGNMENT1_NAME}.coords
+  # Alignment between ref and qry for RagTagShield
+  REFASM_FILENAME=$(basename $REF_ASM)
+  QRYASM_FILENAME=$(basename $QRY_ASM)
+  ALIGNMENT1_NAME=${REFASM_FILENAME%.*}-vs-${QRYASM_FILENAME%.*}-c5000
 
-# RagTag round 1
-# run correct to break contigs if alignment and reads support
-# --debug to provide more info on breaks
-# --intra only breaks when query maps to single ref sequence, 
-#    prevents splitting out unplaced reference scaffolds
-# -f minimum alignment length to consider
-# -t threads
-# --gff defines regions to preserve in qry assembly
-# -T corr for high quality long reads
-# -o output directory
-echo -e "\nStarting RagTag Round 1"
-echo -e "Commands:\n ragtag.py correct --debug --intra -f 5000 -t 32 --gff ${ALIGNMENT1_NAME}.coords-BetterInQry.gff -R $QRY_READS -T corr -o ./ragtag_output-round1 $REF_ASM $QRY_ASM"
-echo -e " ragtag.py scaffold -r -t 32 -o ./ragtag_output-round1 $REF_ASM ragtag_output-round1/ragtag.correct.fasta\n"
+  echo -e "\nRunning genome alignment to identify regions to preserve in $QRY_ASM"
+  echo -e "Commands:\n nucmer -p ${ALIGNMENT1_NAME} --threads=32 -c 5000 ${REF_ASM} ${QRY_ASM}"
+  echo -e " show-coords ${ALIGNMENT1_NAME}.delta > ${ALIGNMENT1_NAME}.coords\n"
 
-ragtag.py correct --debug --intra -f 5000 -t 32 \
-  --gff ${ALIGNMENT1_NAME}.coords-BetterInQry.gff \
-  -R $QRY_READS -T corr \
-  -o ./ragtag_output-round1 \
-  $REF_ASM $QRY_ASM
+  nucmer -p ${ALIGNMENT1_NAME} --threads=32 -c 5000 ${REF_ASM} ${QRY_ASM}
+  show-coords ${ALIGNMENT1_NAME}.delta > ${ALIGNMENT1_NAME}.coords
 
-# run scaffold to reorder corrected QRY scaffolds
-# -r to estimate gap size
-ragtag.py scaffold -r -t 32 -o ./ragtag_output-round1 $REF_ASM ragtag_output-round1/ragtag.correct.fasta
+  # RagTagShield for first round of ragtag
+  echo -e "\nWriting GFF for regions to preserve in $QRY_ASM"
+  echo -e "Command:\n Rscript $GIT_REPOS/FlyComparativeGenomics/RagTagShield-writeGFF.R ${ALIGNMENT1_NAME}.coords"
 
-# A known issue in RagTag can introduce duplicate sequence header names: https://github.com/malonge/RagTag/issues/94#issuecomment-1006880604
-# This case is slightly different.  Not using -u, but the "_RagTag" label on modified scaffolds can still cause duplicates in round 2.
-# Similar issue can also crop up between passes in the larger pipeline (3 total passes, 2 rounds each)
-# Easiest fix is editing this tag in the round1 output. Also has the benefit of tracking which pass and round introduced changes
-echo -e "\nEditing sequence headers in pass $PASS_TAG round 1 output to avoid duplicates in round 2 or future passes"
-echo -e "Command:\n sed -i 's/RagTag/RTp${PASS_TAG}r1/' ragtag_output-round1/ragtag.scaffold.fasta"
+  Rscript $GIT_REPOS/FlyComparativeGenomics/RagTagShield-writeGFF.R ${ALIGNMENT1_NAME}.coords
 
-sed -i "s/RagTag/RTp${PASS_TAG}r1/" ragtag_output-round1/ragtag.scaffold.fasta
+  # RagTag round 1
+  # run correct to break contigs if alignment and reads support
+  # --debug to provide more info on breaks
+  # --intra only breaks when query maps to single ref sequence, 
+  #    prevents splitting out unplaced reference scaffolds
+  # -f minimum alignment length to consider
+  # -t threads
+  # --gff defines regions to preserve in qry assembly
+  # -T corr for high quality long reads
+  # -o output directory
+  echo -e "\nStarting RagTag Round 1"
+  echo -e "Commands:\n ragtag.py correct --debug --intra -f 5000 -t 32 --gff ${ALIGNMENT1_NAME}.coords-BetterInQry.gff -R $QRY_READS -T corr -o ./ragtag_output-round1 $REF_ASM $QRY_ASM"
+  echo -e " ragtag.py scaffold -r -t 32 -o ./ragtag_output-round1 $REF_ASM ragtag_output-round1/ragtag.correct.fasta\n"
 
-# stats for new assembly
-echo -e "\nRunning gfastats and get_pct_chrs.sh on ragtag_output-round1/ragtag.scaffold.fasta"
-sbatch $GIT_REPOS/FlyComparativeGenomics/gfastats.slurm ragtag_output-round1/ragtag.scaffold.fasta
-samtools faidx ragtag_output-round1/ragtag.scaffold.fasta
-$GIT_REPOS/FlyComparativeGenomics/get_pct_chrs.sh ragtag_output-round1/ragtag.scaffold.fasta.fai \
-  > ragtag_round1-pct_chrs.txt
+  ragtag.py correct --debug --intra -f 5000 -t 32 \
+    --gff ${ALIGNMENT1_NAME}.coords-BetterInQry.gff \
+    -R $QRY_READS -T corr \
+    -o ./ragtag_output-round1 \
+    $REF_ASM $QRY_ASM
 
+  # run scaffold to reorder corrected QRY scaffolds
+  # -r to estimate gap size
+  ragtag.py scaffold -r -t 32 -o ./ragtag_output-round1 $REF_ASM ragtag_output-round1/ragtag.correct.fasta
+
+  # A known issue in RagTag can introduce duplicate sequence header names: https://github.com/malonge/RagTag/issues/94#issuecomment-1006880604
+  # This case is slightly different.  Not using -u, but the "_RagTag" label on modified scaffolds can still cause duplicates in round 2.
+  # Similar issue can also crop up between passes in the larger pipeline (3 total passes, 2 rounds each)
+  # Easiest fix is editing this tag in the round1 output. Also has the benefit of tracking which pass and round introduced changes
+  echo -e "\nEditing sequence headers in pass $PASS_TAG round 1 output to avoid duplicates in round 2 or future passes"
+  echo -e "Command:\n sed -i 's/RagTag/RTp${PASS_TAG}r1/' ragtag_output-round1/ragtag.scaffold.fasta"
+
+  sed -i "s/RagTag/RTp${PASS_TAG}r1/" ragtag_output-round1/ragtag.scaffold.fasta
+
+  # stats for new assembly
+  echo -e "\nRunning gfastats and get_pct_chrs.sh on ragtag_output-round1/ragtag.scaffold.fasta"
+  sbatch $GIT_REPOS/FlyComparativeGenomics/gfastats.slurm ragtag_output-round1/ragtag.scaffold.fasta
+  samtools faidx ragtag_output-round1/ragtag.scaffold.fasta
+  $GIT_REPOS/FlyComparativeGenomics/get_pct_chrs.sh ragtag_output-round1/ragtag.scaffold.fasta.fai \
+    > ragtag_round1-pct_chrs.txt
+
+fi
 
 # ROUND 2 - rescaffold round 1 output based on QRY_ASM (original input)
 # RagTagShield will preserve regions of improvement
