8389406827f9d6f84e666ff8f3073cc71aaff322
dluecke <david.luecke@gmail.com>
2025-12-23 10:11:32 -0800

    tag outputs with assembly input name

--- splitasm_non_autosomes.sh
+++ splitasm_non_autosomes.sh
@@ -24,18 +24,18 @@ samtools faidx $IN_ASM
 sort $IN_ASM.fai -nr -k2,2 | \
     cut -f1 | \
     head -n $N_CHR \
-    > scaffolds-autosomes.list
+    > ${IN_ASM%.*}.scaffolds-autosomes.list
 
-seqtk subseq $IN_ASM scaffolds-autosomes.list > ${IN_ASM%.*}.autosomes.fa
+seqtk subseq $IN_ASM ${IN_ASM%.*}.scaffolds-autosomes.list > ${IN_ASM%.*}.autosomes.fa
 
 sort $IN_ASM.fai -nr -k2,2 | \
     cut -f1 | \
     tail -n +$((N_CHR+1)) \
-    > scaffolds-rest.list
+    > ${IN_ASM%.*}.scaffolds-rest.list
 
-seqtk subseq $IN_ASM scaffolds-rest.list > ${IN_ASM%.*}.rest.fa
+seqtk subseq $IN_ASM ${IN_ASM%.*}.scaffolds-rest.list > ${IN_ASM%.*}.rest.fa
 
-ragtag.py splitasm ${IN_ASM%.*}.rest.fa > ${IN_ASM%.*}.rest.split.fa
+ragtag.py splitasm -o ${IN_ASM%.*}.rest.split.agp  ${IN_ASM%.*}.rest.fa > ${IN_ASM%.*}.rest.split.fa
 
 cat ${IN_ASM%.*}.autosomes.fa
 cat ${IN_ASM%.*}.rest.split.fa
