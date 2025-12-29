11b76ec8f897ad776e2e59fb694d2ea1eb4e8d27
dluecke <david.luecke@gmail.com>
2025-12-29 08:21:08 -0800

    streamlined shell script to write BAM for specified region

---
+++ stv-region_bam.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# stv-region_bam.sh quick shell to write BAM file for specified region
+# takes BAM (or SAM) file with headers, and region in samtools view format seq:c1-c2
+# writes BAM "inbamname-seq_c1toc2.bam"
+# requires samtools, written with Ceres module 1.17
+# run from salloc node with samtools loaded
+
+# USAGE: stv-region_bam.sh $IN_BAM "seq:c1-c2"
+
+IN_ALN=$1
+REGION=$2
+
+[[ -n $REGION ]] || \
+  { echo "USAGE: sbatch samtools_view-region.slurm IN.SAM|IN.BAM seq:c1-c2"; exit; }
+
+# convert region syntax seq_N:0-100 to filename syntax seqN_0to100
+REG_TAG=$(echo $REGION | tr -d '_' | tr ':' '_' | sed 's/-/to/')
+
+# output BAM
+OUT_BAM=${IN_ALN%.*}-${REG_TAG}.bam
+
+# check for index of alignment file, use -c to avoid max seq length issue
+if [ ! -f ${IN_ALN}.csi ]; then
+    samtools index -c $IN_ALN
+fi
+
+# write out bam file
+samtools view -b -o $OUT_BAM $IN_ALN $REGION
