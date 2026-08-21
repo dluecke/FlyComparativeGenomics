3646d747a608057d8fab093e849153a6192a1a51
dluecke <david.luecke@gmail.com>
2026-08-21 11:35:47 -0700

    add ragtag for X, assembly stats, program subdirectories ragtag_Xctgs and yahs_Yctgs, log backup

--- Rephase_XY_contigs.sh
+++ Rephase_XY_contigs.sh
@@ -11,13 +11,13 @@
 
 # USAGE Rephase_XY_contigs.sh INPUT.json ASM_NAME_PREFIX
 # INPUT.json is a json file with the following keys:
-#   MALE_DIPLOID: Path to the full male diploid assembly
-#   X_REF: Path to the X reference scaffold
-#   PHASE_X_AUTOSOMES: Path to the list of autosome scaffolds for X phase
-#   PHASE_Y_AUTOSOMES: Path to the list of autosome scaffolds for Y phase
-#   X_CTGS: Path to the list of X contigs
-#   Y_CTGS: Path to the list of Y contigs
-#   HIC_FQ: [Path to HiCR1.fq, Path to HiCR2.fq]
+#   MALE_DIPLOID: Absolute path to the full male diploid assembly
+#   X_REF: Absolute path to the X reference scaffold 
+#   PHASE_X_AUTOSOMES: Absolute path to the list of autosome scaffolds for X phase
+#   PHASE_Y_AUTOSOMES: Absolute path to the list of autosome scaffolds for Y phase
+#   X_CTGS: Absolute path to the list of X contigs
+#   Y_CTGS: Absolute path to the list of Y contigs
+#   HIC_FQ: [Absolute path to HiCR1.fq, Absolute path to HiCR2.fq]
 
 # REQUIREMENTS: samtools
 GIT_REPO=~/FlyComparativeGenomics
@@ -33,6 +33,11 @@ Y_PHASE_ASM_V0="${ASM_ID}-Y_v0.fa"
 INPUT_JSON_FN=$(basename $INPUT_JSON)
 LOGFILE="Rephase_XY_contigs-${INPUT_JSON_FN%.json}.log"
 
+if [[ -f $LOGFILE ]]; then
+  cat $LOGFILE >> "${LOGFILE}.bak"
+  echo -e "\n\n\n" >> "${LOGFILE}.bak"
+fi
+
 date > $LOGFILE
 echo "Starting Rephase_XY_contigs.sh" >> $LOGFILE
 echo "INPUT_JSON: $INPUT_JSON" >> $LOGFILE
@@ -42,11 +47,13 @@ echo
 samtools --version >> $LOGFILE
 
 DIPLOID_ASM=$(jq -r '.MALE_DIPLOID' $INPUT_JSON)
-X_REF_FA=$(jq -r '.X_REF' $INPUT_JSON)
+X_REF_FA=$(jq -r '.X_REF' $INPUT_JSON) 
 X_AUTOSOMES_LIST=$(jq -r '.PHASE_X_AUTOSOMES' $INPUT_JSON)
 Y_AUTOSOMES_LIST=$(jq -r '.PHASE_Y_AUTOSOMES' $INPUT_JSON)
 X_CTGS_LIST=$(jq -r '.X_CTGS' $INPUT_JSON)
 Y_CTGS_LIST=$(jq -r '.Y_CTGS' $INPUT_JSON)
+HIC_FQ1=$(jq -r '.HIC_FQ[0]' $INPUT_JSON)
+HIC_FQ2=$(jq -r '.HIC_FQ[1]' $INPUT_JSON)
 
 echo -e "\nCompiling X phase assembly with autosomes and X contigs" >> $LOGFILE
 echo "CMD: samtools faidx $DIPLOID_ASM -r $X_AUTOSOMES_LIST > $X_PHASE_ASM_V0" >> $LOGFILE
@@ -60,11 +67,29 @@ samtools faidx $DIPLOID_ASM -r $Y_AUTOSOMES_LIST > $Y_PHASE_ASM_V0
 echo "CMD: samtools faidx $DIPLOID_ASM -r $Y_CTGS_LIST >> $Y_PHASE_ASM_V0" >> $LOGFILE
 samtools faidx $DIPLOID_ASM -r $Y_CTGS_LIST >> $Y_PHASE_ASM_V0
 
-echo -e "\nStarting YaHS scaffolding for Y phase assembly" >> $LOGFILE
-HIC_FQ1=$(jq -r '.HIC_FQ[0]' $INPUT_JSON)
-HIC_FQ2=$(jq -r '.HIC_FQ[1]' $INPUT_JSON)
-echo "CMD: sbatch $GIT_REPO/yahs_subset.slurm $Y_PHASE_ASM_V0 $HIC_FQ1 $HIC_FQ2 $Y_CTGS_LIST $ASM_ID-Y" >> $LOGFILE
-sbatch $GIT_REPO/yahs_subset.slurm $Y_PHASE_ASM_V0 $HIC_FQ1 $HIC_FQ2 $Y_CTGS_LIST $ASM_ID"-Y"
+echo -e "\nStatistics for V0 phase assemblies" >> $LOGFILE
+echo "CMD: $GIT_REPO/gfastats.slurm $X_PHASE_ASM_V0" >> $LOGFILE
+echo "CMD: $GIT_REPO/gfastats.slurm $Y_PHASE_ASM_V0" >> $LOGFILE
+sbatch $GIT_REPO/gfastats.slurm $X_PHASE_ASM_V0
+sbatch $GIT_REPO/gfastats.slurm $Y_PHASE_ASM_V0
+
+mkdir -p ragtag_Xctgs
+cd ragtag_Xctgs
+echo -e "\nStarting RagTag scaffolding for X phase assembly in $PWD" >> ../$LOGFILE
+echo "Compiling X contig sequences: ${ASM_ID}-X_ctgs.fa" >> ../$LOGFILE
+echo "CMD: samtools faidx $DIPLOID_ASM -r $X_CTGS_LIST > ${ASM_ID}-X_ctgs.fa" >> ../$LOGFILE
+samtools faidx $DIPLOID_ASM -r $X_CTGS_LIST > ${ASM_ID}-X_ctgs.fa
+echo -e "\nSubmitting RagTag scaffolding for X phase assembly" >> ../$LOGFILE
+echo "CMD: sbatch $GIT_REPO/ragtag_scaffold.slurm $X_REF_FA ${ASM_ID}-X_ctgs.fa" >> ../$LOGFILE
+sbatch $GIT_REPO/ragtag_scaffold.slurm $X_REF_FA ${ASM_ID}-X_ctgs.fa
+cd ..
+
+mkdir yahs_Yctgs
+cd yahs_Yctgs
+echo -e "\nStarting YaHS scaffolding for Y phase assembly" >> ../$LOGFILE
+echo "CMD: sbatch $GIT_REPO/yahs_subset.slurm ../$Y_PHASE_ASM_V0 $HIC_FQ1 $HIC_FQ2 $Y_CTGS_LIST $ASM_ID-Y" >> ../$LOGFILE
+sbatch $GIT_REPO/yahs_subset.slurm ../$Y_PHASE_ASM_V0 $HIC_FQ1 $HIC_FQ2 $Y_CTGS_LIST $ASM_ID"-Y"
+cd ..
 
 echo
 date >> $LOGFILE
