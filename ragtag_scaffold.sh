bcf03561997edfa9c328fdd799bbd1ded8f6b3a8
dluecke <david.luecke@gmail.com>
2026-08-26 12:43:24 -0700

    fix options order typo, add $3 option for threads

--- ragtag_scaffold.sh
+++ ragtag_scaffold.sh
@@ -12,9 +12,10 @@ echo -e "\nRagTag loaded: $(ragtag.py --version)\n"
 
 REF=$1
 QRY=$2
+[ -z $3 ] && THREADS=32 || THREADS=$3
 
 # run correct to break contigs if alignment and reads support
 # -r to estimate gap size, -u to tag unmodified sequences (better for AGP)
-echo "CMD: ragtag.py scaffold -r -u -t 32 $REF $QRY"
-ragtag.py scaffold -r -t -u 32 $REF $QRY
+echo "CMD: ragtag.py scaffold -r -u -t $THREADS $REF $QRY"
+ragtag.py scaffold -r -u -t $THREADS $REF $QRY
 
